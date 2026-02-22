d14040f9e0df23c6f39218f21d36c3aa73cc64f2
David <david11jv@gmail.com>
2026-02-22 11:18:39 +0100

    Initial commit

---
+++ src/main/java/com/davinchicoder/spring_otel/SpringOtelApplication.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.spring_otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOtelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOtelApplication.class, args);
+    }
+
+}
